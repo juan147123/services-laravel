db32da7f034ca30c3f9297b601d3ef37cf8a9b6c
Juan <jcmlmph@gmail.com>
2022-08-09 23:51:22 -0500

    add jwt

--- config/app.php
+++ config/app.php
@@ -161,6 +161,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -230,7 +234,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -6,28 +6,33 @@ use App\Http\Controllers\{
     ProductController,
     BrandController,
     CategoryController,
-    SupplierController};
+    SupplierController
+};
 
-Route::get('/product', [ProductController::class, 'listAll']);
-Route::get('/product/{id}', [ProductController::class, 'findById']);
-Route::post('/product', [ProductController::class, 'create']);
-Route::put('/product/{id}', [ProductController::class, 'update']);
-Route::delete('/product/{id}', [ProductController::class, 'delete']);
+Route::group(['middleware' => ['jwt.verify']], function () {
 
-Route::get('/brand', [BrandController::class, 'listAll']);
-Route::get('/brand/{id}', [BrandController::class, 'findById']);
-Route::post('/brand', [BrandController::class, 'create']);
-Route::put('/brand/{id}', [BrandController::class, 'update']);
-Route::delete('/brand/{id}', [BrandController::class, 'delete']);
+    Route::get('/product', [ProductController::class, 'listAll']);
+    Route::get('/product/{id}', [ProductController::class, 'findById']);
+    Route::post('/product', [ProductController::class, 'create']);
+    Route::put('/product/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'delete']);
+    
+    Route::get('/brand', [BrandController::class, 'listAll']);
+    Route::get('/brand/{id}', [BrandController::class, 'findById']);
+    Route::post('/brand', [BrandController::class, 'create']);
+    Route::put('/brand/{id}', [BrandController::class, 'update']);
+    Route::delete('/brand/{id}', [BrandController::class, 'delete']);
+    
+    Route::get('/category', [CategoryController::class, 'listAll']);
+    Route::get('/category/{id}', [CategoryController::class, 'findById']);
+    Route::post('/category', [CategoryController::class, 'create']);
+    Route::put('/category/{id}', [CategoryController::class, 'update']);
+    Route::delete('/category/{id}', [CategoryController::class, 'delete']);
+    
+    Route::get('/supplier', [SupplierController::class, 'listAll']);
+    Route::get('/supplier/{id}', [SupplierController::class, 'findById']);
+    Route::post('/supplier', [SupplierController::class, 'create']);
+    Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+    Route::delete('/supplier/{id}', [SupplierController::class, 'delete']);
 
-Route::get('/category', [CategoryController::class, 'listAll']);
-Route::get('/category/{id}', [CategoryController::class, 'findById']);
-Route::post('/category', [CategoryController::class, 'create']);
-Route::put('/category/{id}', [CategoryController::class, 'update']);
-Route::delete('/category/{id}', [CategoryController::class, 'delete']);
-
-Route::get('/supplier', [SupplierController::class, 'listAll']);
-Route::get('/supplier/{id}', [SupplierController::class, 'findById']);
-Route::post('/supplier', [SupplierController::class, 'create']);
-Route::put('/supplier/{id}', [SupplierController::class, 'update']);
-Route::delete('/supplier/{id}', [SupplierController::class, 'delete']);
+});
